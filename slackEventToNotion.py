b80799e37e4f0b1019a8927277c0a15a63170134
home <home+spctrm404@users.noreply.github.com>
2021-01-17 05:56:22 +0900

    using delayed process

--- slackEventToNotion.py
+++ slackEventToNotion.py
@@ -1,3 +1,5 @@
+# https://slack-event-to-notion.herokuapp.com/slack/events
+
 from notion.block import TextBlock
 from notion.client import NotionClient
 import os
@@ -6,11 +8,12 @@ from pathlib import Path
 from flask import Flask
 import slack
 from slackeventsapi import SlackEventAdapter
+import threading
 
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# env_path = Path(".") / ".env"
-# load_dotenv(dotenv_path=env_path)
+env_path = Path(".") / ".env"
+load_dotenv(dotenv_path=env_path)
 
 notionUrls = {
     "post": "https://www.notion.so/prsmlab/2057ce5fa809459684ca8e51c4b6d461?v=c05502589aa34e10bb3004318c84916c",
@@ -43,86 +46,101 @@ slackClient = slack.WebClient(token=environ["SLACK_TOKEN"])
 
 ignoreList = []
 cnt = 0
+eventList = []
+
+
+def eventProcess():
+    global eventList
+    if len(eventList) > 0:
+        print("processBegin")
+        print("size:" + str(len(eventList)))
+        event = eventList.pop(0)
+        message = event.get("message")
+        client_msg_id = str(message.get("client_msg_id"))
+        print("processing:" + client_msg_id)
+        channel = event.get("channel")
+        if channel in allowedSlackChannels.values():
+            postContentsCnt = 0
+            postContents = str(message.get("text")).split("\n")
+            newPostRow = postCollection.collection.add_row()
+            for postContent in postContents:
+                if postContent != "":
+                    postText = newPostRow.children.add_new(TextBlock)
+                    postText.title = postContent
+                    postContentsCnt += 1
+                    if postContentsCnt == 1:
+                        newPostRow.ireum = postContent
+            permalink = slackClient.chat_getPermalink(
+                channel=channel, message_ts=message.get("ts")
+            )
+            if permalink.get("ok"):
+                newPostRow.slack_url = str(permalink.get("permalink"))
+            postId = newPostRow.id
+            linkIds = []
+            attachments = message.get("attachments")
+            for atchItem in attachments:
+                newLinkRow = linkCollection.collection.add_row()
+                newLinkRow.translation = "번역없음"
+                newLinkRow.slack_message = [postId]
+                if "author_name" in atchItem:
+                    newLinkRow.author = str(atchItem.get("author_name"))
+                if "original_url" in atchItem:
+                    newLinkRow.url = str(atchItem.get("original_url"))
+                if "title" in atchItem:
+                    newLinkRow.ireum = str(atchItem.get("title"))
+                if "service_icon" in atchItem:
+                    newLinkRow.set(
+                        "format.page_icon",
+                        str(atchItem.get("service_icon")),
+                    )
+                if "image_url" in atchItem:
+                    newLinkRow.set("format.page_cover", str(atchItem.get("image_url")))
+                elif "thumb_url" in atchItem:
+                    newLinkRow.set("format.page_cover", str(atchItem.get("thumb_url")))
+                elif "service_icon" in atchItem:
+                    newLinkRow.set(
+                        "format.page_cover",
+                        str(atchItem.get("service_icon")),
+                    )
+                if "text" in atchItem:
+                    linkContents = str(atchItem.get("text")).split("\n")
+                    for linkContent in linkContents:
+                        if linkContent != "":
+                            linkText = newLinkRow.children.add_new(TextBlock)
+                            linkText.title = linkContent
+                linkIds.append(newLinkRow.id)
+            newPostRow.related_links = linkIds
+            print("procesDone")
+        print("size:" + str(len(eventList)))
+        print("processEnd")
 
 
 @slack_event_adapter.on("message")
 def message(payload):
     global ignoreList
     global cnt
+    global eventList
     event = payload.get("event")
     if "subtype" in event:
         if event.get("subtype") == "message_changed":
             message = event.get("message")
             client_msg_id = str(message.get("client_msg_id"))
             gate = client_msg_id not in ignoreList
-            print("outside: " + str(cnt))
+            print("counter(out): " + str(cnt))
             print("client_msg_id:" + client_msg_id)
             print("ignoreList:" + str(ignoreList))
             print("gate:" + str(gate))
             if gate:
                 ignoreList.append(client_msg_id)
-                print("inside: " + str(cnt))
+                eventList.append(event)
+                print("counter(in): " + str(cnt))
                 print("ignoreList(add):" + str(ignoreList))
                 if len(ignoreList) > 8:
                     ignoreList.pop(0)
                     print("ignoreList(del):" + str(ignoreList))
-                channel = event.get("channel")
-                if channel in allowedSlackChannels.values():
-                    postContentsCnt = 0
-                    postContents = str(message.get("text")).split("\n")
-                    newPostRow = postCollection.collection.add_row()
-                    for postContent in postContents:
-                        if postContent != "":
-                            postText = newPostRow.children.add_new(TextBlock)
-                            postText.title = postContent
-                            postContentsCnt += 1
-                            if postContentsCnt == 1:
-                                newPostRow.ireum = postContent
-                    permalink = slackClient.chat_getPermalink(
-                        channel=channel, message_ts=message.get("ts")
-                    )
-                    if permalink.get("ok"):
-                        newPostRow.slack_url = str(permalink.get("permalink"))
-                    postId = newPostRow.id
-                    linkIds = []
-                    attachments = message.get("attachments")
-                    for atchItem in attachments:
-                        newLinkRow = linkCollection.collection.add_row()
-                        newLinkRow.translation = "번역없음"
-                        newLinkRow.slack_message = [postId]
-                        if "author_name" in atchItem:
-                            newLinkRow.author = str(atchItem.get("author_name"))
-                        if "original_url" in atchItem:
-                            newLinkRow.url = str(atchItem.get("original_url"))
-                        if "title" in atchItem:
-                            newLinkRow.ireum = str(atchItem.get("title"))
-                        if "service_icon" in atchItem:
-                            newLinkRow.set(
-                                "format.page_icon",
-                                str(atchItem.get("service_icon")),
-                            )
-                        if "image_url" in atchItem:
-                            newLinkRow.set(
-                                "format.page_cover", str(atchItem.get("image_url"))
-                            )
-                        elif "thumb_url" in atchItem:
-                            newLinkRow.set(
-                                "format.page_cover", str(atchItem.get("thumb_url"))
-                            )
-                        elif "service_icon" in atchItem:
-                            newLinkRow.set(
-                                "format.page_cover",
-                                str(atchItem.get("service_icon")),
-                            )
-                        if "text" in atchItem:
-                            linkContents = str(atchItem.get("text")).split("\n")
-                            for linkContent in linkContents:
-                                if linkContent != "":
-                                    linkText = newLinkRow.children.add_new(TextBlock)
-                                    linkText.title = linkContent
-                        linkIds.append(newLinkRow.id)
-                    newPostRow.related_links = linkIds
-                    cnt += 1
+                cnt += 1
+                print("counter(fin): " + str(cnt))
+                threading.Timer(5, eventProcess).start()
 
 
 if __name__ == "__main__":
